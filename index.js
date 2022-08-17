f998bd79d763bcae8f7cd840541860b12ee95c32
QunFan <allenfan1012@gmail.com>
2022-08-17 21:35:41 +1000

    commit tree 057accf3e8302e7824ea07569d76cd50107a77c8

---
+++ index.js
@@ -0,0 +1,13 @@
+fi = (function() {
+    return {
+      libraryMethod: function() {
+        return "Start by reading the article!";
+      },
+  
+      each: function() {
+        /*TODO*/
+      }
+    };
+  })();
+  
+  fi.libraryMethod();
